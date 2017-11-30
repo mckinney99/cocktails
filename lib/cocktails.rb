1ea6c3e1f37086c9cec38204ea16c5dce5b2477a
Eric McKinney <mckinney99@gmail.com>
2017-11-29 21:15:02 -0600

    added cli and general functionality

--- lib/cocktails.rb
+++ lib/cocktails.rb
@@ -2,7 +2,5 @@ require 'open-uri'
 require 'pry'
 require 'nokogiri'
 
-
 require "cocktails/version"
 require "cocktails/cli"
-require "cocktails/drink"
--- lib/cocktails/cli.rb
+++ lib/cocktails/cli.rb
@@ -1,36 +1,50 @@
 class Cocktails::CLI
 
   def call
-    puts "Here are the top 50 most popular cocktails worldwide:"
-    list_cocktails
-    more_info
-    goodbye
+    start
   end
 
   def list_cocktails
+    @@all = []
     puts "The most popular cocktails in the world are:"
-    @drink = Cocktails::Drink.list
-  end
-
-  def more_info
-    input = nil
-    while input != "exit"
-      puts "Enter the number of cocktail for more information, or type list for the list, or exit:"
-      input = gets.strip.downcase
-
-      if input.to_i > 0
-        puts @deals[input.to_i-1]
-      elsif input == "list"
-        list_cocktails
-      else
-        puts "Try Again! Type list, or exit."
+    doc = Nokogiri::HTML(open('https://vinepair.com/articles/50-most-popular-cocktails-world-2017/'))
+    names = doc.css("body.category-wine-blog article h3").each do |node|
+      hash = {}
+      hash[:name] = node.text
+      hash[:description] = node.next.next.text
+      if hash[:description] == ""
+        hash[:description] = node.next.next.next.next.text
       end
+      @@all.unshift(hash)
+    end
+    @@all.each do |hash|
+      puts hash[:name]
+    end
   end
-end
 
-  def goodbye
+  def find_info(input)
+    drink = @@all[input.to_i - 1]
+    puts drink[:name]
+    puts drink[:description]
+  end
+
+def start
+  list_cocktails
+  input = nil
+  while input != "exit"
+    puts ""
+    puts "What drink would you like more information on, by number?"
+    puts ""
+    puts "Enter list to see the drinks again."
+    puts "Enter exit to end the program."
+    puts ""
+    input = gets.strip
+    if input == "list"
+      list_cocktails
+    elsif input.to_i > 0
+      find_info(input)
+      end
+    end
     puts "Goodbye!"
   end
-
-
 end
